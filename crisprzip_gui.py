f61340a3dc876258516342df23a75d7d297238fd
edvinskis <E.Dvinskis@tudelft.nl>
2025-03-26 17:04:13 +0100

    Update in vitro cleavage tab functionality

--- crisprzip_gui.py
+++ crisprzip_gui.py
@@ -1,26 +1,143 @@
 from nicegui import ui
-import crisprzip
+from crisprzip import *
 import plotly.graph_objects as go
 import numpy as np
 import json
 import os
+from plotly.subplots import make_subplots
+from scipy.optimize import curve_fit
+from crisprzip.kinetics import *
 
 ###############################################################################
 # Helper functions: CRISPRzip predictions
 ###############################################################################
 
-# Load models (lanscape params)
-def load_model_params(model_name: str) -> dict:
-    """Load parameters from CRISPRzip JSON files"""
-    model_map = {
-        'sequence-params2': 'sequence_params.json',
-        'average-params': 'average_params.json',
-        'average-params-legacy': 'average_params_legacy.json'
-    }
+# Not needed
+# # Load models (lanscape params)
+# def load_model_params(model_name: str) -> dict:
+#     """Load parameters from CRISPRzip JSON files"""
+#     model_map = {
+#         'sequence-params2': 'sequence_params.json',
+#         'average-params': 'average_params.json',
+#         'average-params-legacy': 'average_params_legacy.json'
+#     }
     
-    base_path = os.path.join(os.path.dirname(crisprzip.__file__), 'landscape_params/')
-    with open(os.path.join(base_path, model_map[model_name])) as f:
-        return json.load(f)['param_values']
+#     base_path = os.path.join(os.path.dirname(crisprzip.__file__), 'landscape_params/')
+#     with open(os.path.join(base_path, model_map[model_name])) as f:
+#         return json.load(f)['param_values']
+    
+# Function to get cleavage rate
+def get_cleavage_rate(stc, binding_rate):
+    dt = np.logspace(-2, 6)
+    f_clv = stc.get_cleaved_fraction(dt, binding_rate)
+    with np.errstate(over='ignore'): # ignore RuntimeWarning: overflow
+        k_eff = curve_fit(
+            f=lambda t, k: 1 - np.exp(-k * t),
+            xdata=dt,
+            ydata=f_clv,
+        )[0][0]
+    return k_eff
+
+# Submit button handler for in vitro cleavage
+def submit_handler_in_vitro_cleavage():
+    try:
+        parameter_set = model_dropdown.value
+        bare_protein = load_landscape(parameter_set)
+        
+        protospacer = target_sequence_input.value
+        off_targets = [seq.strip() for seq in off_targets_input.value.split(',')]
+        targets = [protospacer] + off_targets
+        
+        guided_protein = bare_protein.bind_guide_rna(protospacer=protospacer)
+        
+        protein_sequence_complexes = []
+        for target_seq in targets:
+            protein_sequence_complexes += [guided_protein.probe_sequence(target_seq)]
+        
+        concentration = float(rnp_concentration_input.value)
+        k_on_ref = 1E-2
+        binding_rate = k_on_ref * concentration
+
+        # Clear previous output content - [IMPORTANT], otherwise plots will stack below on each request
+        output_container.clear()
+        
+        # Targets cleaved over time - needs adjusting and not displayed currently
+        # fig_tt = go.Figure()
+        # stc0 = protein_sequence_complexes[0]
+        # kfit0 = get_cleavage_rate(stc0, k_on_ref)
+        # x_values = np.logspace(-2, 6)
+        
+        # fig_tt.add_trace(go.Scatter(
+        #     x=x_values,
+        #     y=stc0.get_cleaved_fraction(x_values, k_on_ref),
+        #     mode='markers',
+        # ))
+        
+        # fig_tt.add_trace(go.Scatter(
+        #     x=x_values,
+        #     y=1 - np.exp(-kfit0 * x_values),
+        #     mode='lines',
+        #     name=f'exp. fit (k={kfit0:.2e} s⁻¹)'
+        # ))
+        
+        # fig_tt.update_layout(
+        #     xaxis_type="log",
+        #     xaxis_title="time (s)",
+        #     yaxis_title="fraction cleaved f_clv",
+        #     legend=dict(font=dict(size=10)),
+        #     height=300,
+        #     margin=dict(l=50, r=50, t=30, b=50)
+        # )
+        #
+        # Cleaved fraction vs time
+        fig1 = go.Figure()
+        dt = np.logspace(-1, np.log10(7200))
+        
+        for i, stc in enumerate(protein_sequence_complexes):
+            f_clv = stc.get_cleaved_fraction(dt, binding_rate)
+            fig1.add_trace(go.Scatter(
+                x=dt, y=f_clv,
+                name=('on-target' if i == 0 else f'off-target {i}'),
+                mode='lines')
+            )
+        fig1.update_layout(
+            title='Cleaved fraction vs time',
+            xaxis_type="log",
+            xaxis_title="time (s)",
+            yaxis_title="fraction cleaved (f_clv)",
+            height=400,
+            margin=dict(l=50, r=50, t=50, b=50)
+        )
+        # Cleaved rate vs time
+        fig2 = go.Figure()
+        # Concentration range, currently hardcoded and needs a better solution
+        conc_min = .1  # nM
+        conc_max = 100  # nM
+        dc = np.logspace(np.log10(conc_min), np.log10(conc_max))
+        
+        for i, stc in enumerate(protein_sequence_complexes):
+            k_fit = [get_cleavage_rate(stc, k_on_ref * concentration) for concentration in dc]
+            fig2.add_trace(go.Scatter(
+                x=dc, y=k_fit,
+                name=('on-target' if i == 0 else f'off-target {i}'),
+                mode='lines')
+                )
+        fig2.update_layout(
+            title='Cleaved rate vs time',
+            xaxis_type="log",
+            yaxis_type="log",
+            xaxis_title="Cas9 concentration (nM)",
+            yaxis_title="cleavage rate (k_clv)",
+            height=400,
+            margin=dict(l=50, r=50, t=50, b=50)
+        )
+        with output_container:
+                with ui.row().classes('w-full no-wrap'):
+                        ui.plotly(fig1).classes('w-1/2')
+                        ui.plotly(fig2).classes('w-1/2')        
+    except Exception as e:
+        ui.notify(f'Error: {str(e)}', type='negative')
+
 
 ###############################################################################
 # GUI layout with NiceGUI
@@ -71,7 +188,7 @@ with ui.tab_panels(tabs, value=one).classes('w-full'):
                         with ui.row().classes('w-full h-full no-wrap'):
                             ui.markdown('**Target sequence:**').style('font-size: 16px')
                             # Input field for target sequence
-                            target_sequence_input = ui.input(placeholder='Enter target sequence').classes('w-full')
+                            target_sequence_input = ui.input(placeholder='Enter target sequence (e.g., GACGCATAAAGATGAGACGCTGG)').classes('w-full')
                             # Radio buttons to select between "protospacer" and "guide RNA"
                             def update_placeholder(value):
                                 if value == 'protospacer':
@@ -86,46 +203,67 @@ with ui.tab_panels(tabs, value=one).classes('w-full'):
                         # Insert spacer
                         ui.markdown('---')
 
-                        # Off-target sequences textarea and upload button
+                        # # Off-target sequences textarea and upload button
+                        # with ui.row().classes('w-full h-full no-wrap'):
+                        #     ui.markdown('**Off-target sequences:**').style('font-size: 16px')
+                        #     off_target_textarea = ui.textarea(placeholder='Off-target sequence(-es)').props('rows=3').classes('w-1/2')
+                        #     upload_button = ui.button('Upload').props('icon=upload').classes('w-1/3')
+                        #     # Information balloon for off-target sequence
+                        #     ui.icon('info').tooltip('Upload off-target sequences from a local text file.').style('font-size: 20px')
+
+                        # Off-target sequences input
                         with ui.row().classes('w-full h-full no-wrap'):
                             ui.markdown('**Off-target sequences:**').style('font-size: 16px')
-                            off_target_textarea = ui.textarea(placeholder='Off-target sequence(-es)').props('rows=3').classes('w-1/2')
-                            upload_button = ui.button('Upload').props('icon=upload').classes('w-1/3')
-                            # Information balloon for off-target sequence
-                            ui.icon('info').tooltip('Upload off-target sequences from a local text file.').style('font-size: 20px')
-     
+                            off_targets_input = ui.textarea(placeholder='Enter up to 5 off-target sequences, comma-separated').props('rows=3').classes('w-1/2')
+
+                            # Function to validate and split input
+                            def validate_off_targets(text):
+                                sequences = [seq.strip() for seq in text.split(',')]
+                                if len(sequences) > 5:
+                                    ui.notify('Error: Limit exceeded. Only 5 sequences currently allowed.', type='negative')
+                                    return ','.join(sequences[:5])  # Return only the first 5 sequences
+                                return text
+
+                            # Attach the validation function to the input
+                            off_targets_input.on('update:model-value', lambda e: off_targets_input.set_value(validate_off_targets(off_targets_input.value)))
+
+                            # Upload button for file-based input (currently disabled), placeholder bnelow
+                            # upload_button = ui.button('Upload').props('icon=upload').classes('w-1/3')
+                            ui.icon('info').tooltip('Specify off-target sequences. You can check to up to 5 off-target sequences, comma-separated.').style('font-size: 20px')
+
                         # Insert spacer
                         ui.markdown('---')
 
                         # Model dropdown with information balloon. Then next to it, RNP conc.
                         with ui.row().classes('w-full h-full no-wrap'):
-                            # 1. Model label
+                            # Model label
                             ui.markdown('**Model:**').style('font-size: 16px')
 
-                            # 2. Simple dropdown with model names
+                            # Simple dropdown with model names
                             model_dropdown = ui.select(
                                 options={
-                                    'sequence-params2': 'sequence-params2 (recommended)',
-                                    'average-params': 'average-params', 
-                                    'average-params-legacy': 'average-params-legacy'
+                                    'sequence_params': 'sequence-params2 (recommended)',
+                                    'average_params': 'average-params', 
+                                    'average_params_legacy': 'average-params-legacy'
                                 },
-                                value='sequence-params2'
+                                value='sequence_params'
                             ).classes('w-1/4')
 
-                            # 3. Function to load parameters silently
-                            def update_params():
-                                global loaded_params
-                                try:
-                                    selected_value = model_dropdown.value
-                                    loaded_params = load_model_params(selected_value)
-                                    # Optional small notification
-                                    ui.notify('Parameters loaded', type='positive', duration=1)
-                                except Exception as exc:
-                                    ui.notify(f'Error: {str(exc)}', type='negative')
+                            # Not needed
+                            # # 3. Function to load parameters silently
+                            # def update_params():
+                            #     global loaded_params
+                            #     try:
+                            #         selected_value = model_dropdown.value
+                            #         loaded_params = load_model_params(selected_value)
+                            #         # Optional small notification
+                            #         ui.notify('Parameters loaded', type='positive', duration=1)
+                            #     except Exception as exc:
+                            #         ui.notify(f'Error: {str(exc)}', type='negative')
                             
-                            # 4. Connect the event handler
-                            model_dropdown.on('update:model-value', lambda _: update_params())
-                            # Next row has an infi icon
+                            # # 4. Connect the event handler
+                            # model_dropdown.on('update:model-value', lambda _: update_params())
+                            # Next row has an info icon
                             ui.icon('info').tooltip('Select the model for cleavage predictions. Recommended: sequence-params2.').style('font-size: 20px')
                             # Insert spacer
                             ui.markdown('<br>')
@@ -133,14 +271,15 @@ with ui.tab_panels(tabs, value=one).classes('w-full'):
                             ui.markdown('**RNP concentration (nM):**').style('font-size: 16px')
                             rnp_concentration_input = ui.input(value='100').props('type=number').classes('ml-2 w-20')
                         
-                        # Submit button (one row)
-                        submit_button = ui.button('Submit').props('icon=send')
+                        # Submit button with event handler
+                        submit_button = ui.button('Submit', on_click=submit_handler_in_vitro_cleavage).props('icon=send')
 
                     # Vertical separator between inputs and outputs
                     ui.separator().props('vertical')
 
                     with ui.column().classes('w-3/5 h-full no-wrap'):
                         ui.markdown('<u>**Output**</u>').style('font-size: 20px; color: gray;')
+                        output_container = ui.element('div').classes('w-full no-wrap')
                     
 ###############################################################################    
                 # Content for in vitro - binding tab
