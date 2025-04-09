fdcfc51ea26160e561c715599d9036c094db4894
Hidde Offerhaus <H.S.Offerhaus@tudelft.nl>
2025-04-09 16:41:54 +0200

    Ideas for aligning the input

---
+++ content/vitro_cleavage_input_aligned.py
@@ -0,0 +1,88 @@
+from nicegui import ui
+
+def show():
+
+    show_borders = False
+    bc = ' border' if show_borders else ''
+
+    with ui.card().classes('p-4'):
+        with ui.grid(columns=2).style('grid-template-columns: 300px 100px').classes('gap-0'):
+
+            # TARGET SEQUENCE
+            with ui.row(align_items='center').classes('p-0' + bc):
+                ui.markdown('**target sequence**').classes('p-0 leading-[0.7]')
+                ui.icon('info').tooltip(
+                    'Select the model for cleavage predictions. Recommended: sequence-params2.').style(
+                    'font-size: 20px')
+            ui.element()
+
+            with ui.column().classes('w-full p-0' + bc):
+                target_sequence_input = ui.input(
+                    placeholder='GACGCATAAAGATGAGACGCTGG'
+                ).classes('w-[280px] font-mono').props('dense')
+                ui.element().classes("h-4")
+
+            with ui.column().classes('w-full p-0' + bc):
+                target_input_select = ui.select(
+                    ['protospacer', 'guide RNA'],
+                    value='protospacer').classes('w-[100px]').props('dense')
+
+            # OFF-TARGET SEQUENCES
+            with ui.row(align_items='center').classes('p-0' + bc):
+                ui.markdown('**off-target sequence**').classes('p-0 leading-[0.7]')
+                ui.icon('info').tooltip(
+                    'Select the model for cleavage predictions. Recommended: sequence-params2.').style(
+                    'font-size: 20px')
+            ui.element()
+
+            with ui.column().classes('w-full h-full p-0' + bc):
+                with ui.row(align_items='center'):
+                    off_targets_input = ui.textarea(
+                        placeholder='GACGCATAAAGATGAGACGCTGG,\nGACGCATAAAGATGAGACGCTGG,\n...'
+                    ).props('rows=5 dense').classes('w-[280px] h-2fr font-mono')
+                ui.element('h-1')
+            with ui.row(align_items='start').classes('w-full h-full p-0' + bc):
+                with ui.row(align_items='center').classes('w-full h-[52px]'):
+                    ui.button('upload').props('outline no-caps')
+
+            # CONCENTRATION
+            with ui.element().classes('w-full h-full p-0' + bc):
+                with ui.row(align_items='center').classes('w-[280px] gap-0'):
+                    ui.markdown('**RNP concentration**').classes('w-1/2')
+                    concentration_input = (ui.input(placeholder='100')
+                                           .props('dense suffix="nM"')
+                                           .classes('w-1/2'))
+                ui.element().classes("h-4")
+
+            ui.element()
+
+            # PARAMETER SELECTION
+            with ui.column().classes('w-full gap-0 p-0' + bc):
+
+
+                with ui.row(align_items='center').classes('w-full'):
+                    ui.markdown('**model parameters**').classes('leading-[0.7]')
+                    (ui.icon('info')
+                    .tooltip('Select the model for cleavage predictions.')
+                    .style('font-size: 20px'))
+
+                parameter_set_input = ui.select(
+                    options={
+                        'sequence_params': 'sequence-params2 (recommended)',
+                        'average_params': 'average-params',
+                        'average_params_legacy': 'average-params-legacy'
+                    },
+                    value='sequence_params'
+                ).props('dense').classes('w-[280px] p-0 m-0')
+
+                ui.element().classes("h-6")
+
+            ui.element()
+
+            submit_button = ui.button('Submit').props('icon=send').classes('w-[280px]')
+
+@ui.page("/")
+def index():
+    show()
+
+ui.run()
