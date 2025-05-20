63fd769c83365734f601005848fb2048e9311ef3
Hidde Offerhaus <H.S.Offerhaus@tudelft.nl>
2025-05-20 16:14:36 +0200

    Updated layout. No vitro/vivo but only binding/cleavage

--- crisprzip_gui.py
+++ crisprzip_gui.py
@@ -1,5 +1,7 @@
 from nicegui import ui
 import content.vitro_cleavage
+import content.vitro_binding
+
 
 @ui.page('/')
 def index():
@@ -19,36 +21,17 @@ def index():
 
     # TABS
     with ui.tabs().classes('w-full bg-gradient-to-b from-white via-white to-[#6ECFF6] rounded-xl') as tabs:
-        one = ui.tab('in vitro', icon='science')
-        two = ui.tab('in vivo', icon='pest_control_rodent')
+        one = ui.tab('cleavage', icon='content_cut')
+        two = ui.tab('binding', icon='link')
 
     with ui.tab_panels(tabs, value=one).classes('w-full'):
 
         # TAB 1 - IN VITRO
         with ui.tab_panel(one):
-
-            with ui.tabs().classes('justify-start') as in_vitro_tabs:
-                vitro_cleavage = ui.tab('cleavage (Cas9)', icon='content_cut').props('no-caps')
-                vitro_binding = ui.tab('binding (dCas9)', icon='link').props('no-caps')
-
-            with ui.tab_panels(in_vitro_tabs, value=vitro_cleavage).classes('w-full'):
-                with ui.tab_panel(vitro_cleavage).classes('w-full h-full no-wrap'):
-                    content.vitro_cleavage.show_contents()  # content/vitro_cleavage.py
-                with ui.tab_panel(vitro_binding):
-                    ui.label('IN VITRO - BINDING (dCas9) GOES HERE')
+            content.vitro_cleavage.show_contents()  # content/vitro_cleavage.py
 
         # TAB 2 - IN VIVO
         with ui.tab_panel(two):
-
-            with ui.tabs().classes('justify-start') as in_vivo_tabs:
-                vivo_cleavage = ui.tab('cleavage (Cas9)', icon='content_cut').props('no-caps')
-                vivo_binding = ui.tab('binding (dCas9)', icon='link').props('no-caps')
-
-            with ui.tab_panels(in_vivo_tabs, value=vivo_cleavage).classes('w-full'):
-                with ui.tab_panel(vivo_cleavage):
-                    ui.label('IN VIVO - CLEAVAGE (Cas9) PLACEHOLDER')
-                with ui.tab_panel(vivo_binding):
-                    ui.label('IN VIVO - BINDING (dCas9) PLACEHOLDER')
-
+            content.vitro_binding.show_contents()  # content/vitro_binding.py
 
 ui.run()
