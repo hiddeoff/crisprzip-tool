b8fddcac85702d2470915463f0c20faf89c2d5da
Raul Ortiz <raul.ortiz@tudelft.nl>
2025-06-10 16:05:14 +0200

    add instructions to build linux distro

---
+++ bin/build_linos.sh
@@ -0,0 +1,14 @@
+lib_path="$HOME/Documents/CRISPRzip/crisprzip-tool/crisprzip_venv/lib/python3.12/"
+
+pyinstaller crisprzip_gui.py \
+  --name CRISPRzip \
+  --windowed \
+  --onefile \
+  --add-data "$lib_path/site-packages/nicegui:nicegui/static" \
+  --add-data "$lib_path/site-packages/latex2mathml:latex2mathml" \
+  --collect-all nicegui \
+  --collect-all crisprzip \
+  --collect-all matplotlib \
+  --collect-all numpy \
+  --collect-all pandas \
+  --hidden-import uvicorn.logging
